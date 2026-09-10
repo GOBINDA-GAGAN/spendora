2b63be7afbf00d8d5ffdafa2be9cccaec54b6bf9
GOBINDA-GAGAN <gobindagagandey@gmail.com>
2026-09-10 22:34:00 +0530

    feat: add CORS middleware with dynamic client URL configuration

--- server/src/app/app.js
+++ server/src/app/app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import authRouter from "../router/auth.routes.js";
 import cookieParser from "cookie-parser";
+import config from "../config/env.config.js";
+import cors from "cors";
 
 
 
@@ -9,6 +11,15 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: config.clientUrl,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 app.get("/", (req, res) => {
   res.json({
